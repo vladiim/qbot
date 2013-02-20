ba6937731b5aeb0c32a31725b13f7090ae8f015f
vladiim <vladiim@yahoo.com.au>
2013-02-20 13:55:25 +1100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qbot::Application.config.secret_token = '5564715be7eb49f1ead46bf6e03469bfd30adf09034afa4e2c10270551fdab3f01bb647bd456ed541b40ba63c70dd5323cd8851f8467438e4e95d4b816d6b355'
